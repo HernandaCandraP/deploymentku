3026b4f6f72d873cb355f63f7946dbb2dba3aaf8
HernandaCandraP <hernandacp@gmail.com>
2019-05-10 21:02:07 +0700

    add

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/deploymentku'));
+app.use(express.static(__dirname + '/dist'));
 
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/deploymentku/index.html');
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
